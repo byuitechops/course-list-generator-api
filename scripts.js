b4df6e8c994e7d753149f4ac8afceeefda54d7ca
Harker <harkerb2@byui.edu>
2017-09-11 16:22:18 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,58 @@
+function getSemesters(callback){
+    var domXhr = new XMLHttpRequest
+        domXhr.open("GET", "/d2l/api/lp/1.15/enrollments/myenrollments/?orgUnitTypeId=5")
+        domXhr.onload = function(e) {
+            if(domXhr.status == 200){
+                callback(null, JSON.parse(domXhr.response).Items)
+            } else {
+                callback(e, null)
+            }
+        }
+        domXhr.send()
+    }
+
+function getDomain(){
+    var domains = document.getElementsByName('domain')
+    for (i=0; i<domains.length; i++){
+        if (domains[i].checked){
+            return(domains[i].id)
+        }
+    }
+}
+
+
+function filterSemesters(err, semesters){    
+    var domain = getDomain()
+    console.log(semesters)
+    console.log("Selected Domain: " + domain)
+    updateList(semesters)
+}
+
+function updateList(semesters){
+    var stringList = ""
+    semesters.forEach(function(each){
+        stringList += "<option value='"+ each.OrgUnit.Id +"'>" + each.OrgUnit.Name + "</option>"
+    })
+    document.getElementById('semesterList').disabled = false
+    document.getElementById('semesterList').innerHTML = stringList
+}
+
+
+function updateSemesters(){
+    getSemesters(filterSemesters)
+}
+
+
+
+/*display list*/
+function querySemester(){
+    var semesterId = document.getElementById('semesterList').value
+    var semesterXhr = new XMLHttpRequest
+    semesterXhr.open("GET", "/d2l/api/lp/1.15/enrollments/myenrollments/?orgUnitTypeId=3")
+    semesterXhr.onload = function(e){
+        if(semesterXhr.status == 200){
+            console.log(JSON.parse(semesterXhr.response))
+        }
+    }
+    semesterXhr.send();
+}
